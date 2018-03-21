4c18d4f663187cf3453e7e1165a8ee955e7aa219
Wojciech Karas <wkaras89@gmail.com>
2018-03-21 22:34:40 +0100

    change gallery order

--- src/client/pages/gallery-data.ts
+++ src/client/pages/gallery-data.ts
@@ -8,199 +8,205 @@ export type GalleryData = {
   name: string
   photos: GalleryPhoto[]
 }
+
+
+const aroundHouse = {
+  id: "wokoldomu",
+  name: "Wokół domu",
+  photos: [
+    {
+      src: '/photos/after-renovation/IMG_20170528_103157374-x0.15.jpg',
+      width: 624,
+      height: 468
+    },
+    { 
+      src: '/photos/after-renovation/IMG_20170528_103224692-x0.15.jpg',
+      width: 624,
+      height: 468 
+    },
+    {
+      src: '/photos/after-renovation/IMG_20170528_103421649-x0.15.jpg',
+      width: 624,
+      height: 468
+    },
+    { 
+      src: '/photos/after-renovation/IMG_20170528_110910900-x0.15.jpg',
+      width: 624,
+      height: 468
+    },
+    { 
+      src: '/photos/after-renovation/IMG_20170528_111149292-x0.15.jpg',
+      width: 624,
+      height: 468 
+    },
+    { 
+      src: '/photos/after-renovation/IMG_20170528_112001922-x0.15.jpg',
+      width: 624,
+      height: 468 
+    },
+    { 
+      src: '/photos/after-renovation/IMG_20170528_112410108_HDR-x0.15.jpg',
+      width: 624,
+      height: 468
+    },
+    { 
+      src: '/photos/after-renovation/IMG_20170528_112535582-x0.15.jpg',
+      width: 624,
+      height: 468
+    },
+    { 
+      src: '/photos/after-renovation/IMG_20170528_112702189_HDR-x0.15.jpg',
+      width: 624,
+      height: 468 
+    },
+    { 
+      src: '/photos/after-renovation/IMG_20170818_081657059-x0.15.jpg',
+      width: 624,
+      height: 468
+    },
+    { 
+      src: '/photos/after-renovation/IMG_20170818_081724226-x0.15.jpg',
+      width: 624,
+      height: 468 
+    },
+    {  
+      src: '/photos/after-renovation/IMG_20170818_081746427-x0.15.jpg',
+      width: 624,
+      height: 468 
+    },
+    { 
+      src: '/photos/after-renovation/IMG_20170818_162536009_HDR-x0.15.jpg',
+      width: 624,
+      height: 468 
+    },
+    { 
+      src: '/photos/after-renovation/IMG_20170818_162722683-x0.15.jpg',
+      width: 624,
+      height: 468 
+    },
+    { 
+      src: '/photos/after-renovation/IMG_20170818_162927284-x0.15.jpg',
+      width: 468,
+      height: 624 
+    },
+    { 
+      src: '/photos/after-renovation/IMG_20170821_140937381-x0.15.jpg',
+      width: 624,
+      height: 468 
+    },
+    {
+      src: "/photos/summer/yard-4-x0.25.jpg",
+      width: 536,
+      height: 402,
+    },
+    {
+      src: "/photos/summer/yard-8-x0.25.jpg",
+      width: 768,
+      height: 576,
+    },
+    {
+      src: "/photos/summer/yard-9-x0.25.jpg",
+      width: 768,
+      height: 576,
+    },
+    {
+      src: "/photos/summer/backyard-1-x0.25.jpg",
+      width: 536,
+      height: 402,
+    },
+    {
+      src: "/photos/summer/backyard-2-x0.25.jpg",
+      width: 536,
+      height: 402,
+    },
+    {
+      src: "/photos/summer/backyard-3-x0.25.jpg",
+      width: 536,
+      height: 402,
+    },
+    {
+      src: "/photos/summer/backyard-4-x0.25.jpg",
+      width: 460,
+      height: 816,
+    },
+    {
+      src: "/photos/summer/backyard-5-x0.25.jpg",
+      width: 816,
+      height: 460,
+    },
+    {
+      src: "/photos/summer/backyard-6-x0.25.jpg",
+      width: 816,
+      height: 460,
+    },
+  ],
+}
+
+const winter =   {
+  id: "zima",
+  name: "Zima",
+  photos: [
+    {
+      src: "/photos/winter/winter-building-from-front-1-x0.15.jpg",
+      width: 624,
+      height: 351,
+    },
+    {
+      src: "/photos/winter/winter-building-from-front-2-x0.15.jpg",
+      width: 624,
+      height: 468,
+    },
+    {
+      src: "/photos/winter/winter-building-from-lake-1-x0.15.jpg",
+      width: 624,
+      height: 468,
+    },
+    {
+      src: "/photos/winter/winter-building-from-lake-2-x0.15.jpg",
+      width: 624,
+      height: 351,
+    },
+    {
+      src: "/photos/winter/winter-building-from-road-1-x0.15.jpg",
+      width: 624,
+      height: 468,
+    },
+    {
+      src: "/photos/winter/winter-building-from-road-2-x0.15.jpg",
+      width: 624,
+      height: 351,
+    },
+    {
+      src: "/photos/winter/winter-building-from-road-3-x0.15.jpg",
+      width: 624,
+      height: 351,
+    },
+    {
+      src: "/photos/winter/winter-building-from-road-4-x0.15.jpg",
+      width: 624,
+      height: 351,
+    },
+    {
+      src: "/photos/winter/winter-building-from-side-1-x0.15.jpg",
+      width: 624,
+      height: 468,
+    },
+    {
+      src: "/photos/winter/winter-building-from-side-2-x0.15.jpg",
+      width: 624,
+      height: 351,
+    },
+    {
+      src: "/photos/winter/winter-forest-1-x0.15.jpg",
+      width: 624,
+      height: 351,
+    },
+  ],
+}
+
 export const galleryData: GalleryData[] = [
-  {
-    id: "zima",
-    name: "Zima",
-    photos: [
-      {
-        src: "/photos/winter/winter-building-from-front-1-x0.15.jpg",
-        width: 624,
-        height: 351,
-      },
-      {
-        src: "/photos/winter/winter-building-from-front-2-x0.15.jpg",
-        width: 624,
-        height: 468,
-      },
-      {
-        src: "/photos/winter/winter-building-from-lake-1-x0.15.jpg",
-        width: 624,
-        height: 468,
-      },
-      {
-        src: "/photos/winter/winter-building-from-lake-2-x0.15.jpg",
-        width: 624,
-        height: 351,
-      },
-      {
-        src: "/photos/winter/winter-building-from-road-1-x0.15.jpg",
-        width: 624,
-        height: 468,
-      },
-      {
-        src: "/photos/winter/winter-building-from-road-2-x0.15.jpg",
-        width: 624,
-        height: 351,
-      },
-      {
-        src: "/photos/winter/winter-building-from-road-3-x0.15.jpg",
-        width: 624,
-        height: 351,
-      },
-      {
-        src: "/photos/winter/winter-building-from-road-4-x0.15.jpg",
-        width: 624,
-        height: 351,
-      },
-      {
-        src: "/photos/winter/winter-building-from-side-1-x0.15.jpg",
-        width: 624,
-        height: 468,
-      },
-      {
-        src: "/photos/winter/winter-building-from-side-2-x0.15.jpg",
-        width: 624,
-        height: 351,
-      },
-      {
-        src: "/photos/winter/winter-forest-1-x0.15.jpg",
-        width: 624,
-        height: 351,
-      },
-    ],
-  },
-  {
-    id: "wokoldomu",
-    name: "Wokół domu",
-    photos: [
-      {
-        src: '/photos/after-renovation/IMG_20170528_103157374-x0.15.jpg',
-        width: 624,
-        height: 468
-      },
-      { 
-        src: '/photos/after-renovation/IMG_20170528_103224692-x0.15.jpg',
-        width: 624,
-        height: 468 
-      },
-      {
-        src: '/photos/after-renovation/IMG_20170528_103421649-x0.15.jpg',
-        width: 624,
-        height: 468
-      },
-      { 
-        src: '/photos/after-renovation/IMG_20170528_110910900-x0.15.jpg',
-        width: 624,
-        height: 468
-      },
-      { 
-        src: '/photos/after-renovation/IMG_20170528_111149292-x0.15.jpg',
-        width: 624,
-        height: 468 
-      },
-      { 
-        src: '/photos/after-renovation/IMG_20170528_112001922-x0.15.jpg',
-        width: 624,
-        height: 468 
-      },
-      { 
-        src: '/photos/after-renovation/IMG_20170528_112410108_HDR-x0.15.jpg',
-        width: 624,
-        height: 468
-      },
-      { 
-        src: '/photos/after-renovation/IMG_20170528_112535582-x0.15.jpg',
-        width: 624,
-        height: 468
-      },
-      { 
-        src: '/photos/after-renovation/IMG_20170528_112702189_HDR-x0.15.jpg',
-        width: 624,
-        height: 468 
-      },
-      { 
-        src: '/photos/after-renovation/IMG_20170818_081657059-x0.15.jpg',
-        width: 624,
-        height: 468
-      },
-      { 
-        src: '/photos/after-renovation/IMG_20170818_081724226-x0.15.jpg',
-        width: 624,
-        height: 468 
-      },
-      {  
-        src: '/photos/after-renovation/IMG_20170818_081746427-x0.15.jpg',
-        width: 624,
-        height: 468 
-      },
-      { 
-        src: '/photos/after-renovation/IMG_20170818_162536009_HDR-x0.15.jpg',
-        width: 624,
-        height: 468 
-      },
-      { 
-        src: '/photos/after-renovation/IMG_20170818_162722683-x0.15.jpg',
-        width: 624,
-        height: 468 
-      },
-      { 
-        src: '/photos/after-renovation/IMG_20170818_162927284-x0.15.jpg',
-        width: 468,
-        height: 624 
-      },
-      { 
-        src: '/photos/after-renovation/IMG_20170821_140937381-x0.15.jpg',
-        width: 624,
-        height: 468 
-      },
-      {
-        src: "/photos/summer/yard-4-x0.25.jpg",
-        width: 536,
-        height: 402,
-      },
-      {
-        src: "/photos/summer/yard-8-x0.25.jpg",
-        width: 768,
-        height: 576,
-      },
-      {
-        src: "/photos/summer/yard-9-x0.25.jpg",
-        width: 768,
-        height: 576,
-      },
-      {
-        src: "/photos/summer/backyard-1-x0.25.jpg",
-        width: 536,
-        height: 402,
-      },
-      {
-        src: "/photos/summer/backyard-2-x0.25.jpg",
-        width: 536,
-        height: 402,
-      },
-      {
-        src: "/photos/summer/backyard-3-x0.25.jpg",
-        width: 536,
-        height: 402,
-      },
-      {
-        src: "/photos/summer/backyard-4-x0.25.jpg",
-        width: 460,
-        height: 816,
-      },
-      {
-        src: "/photos/summer/backyard-5-x0.25.jpg",
-        width: 816,
-        height: 460,
-      },
-      {
-        src: "/photos/summer/backyard-6-x0.25.jpg",
-        width: 816,
-        height: 460,
-      },
-    ],
-  },
+  aroundHouse,
+  winter,
   {
     id: "staw",
     name: "Staw",
